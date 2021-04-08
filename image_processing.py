94ff9f9a9030b2f6a5f0ca2bcdd97369f24627d7
Ordgod <34941987+Ordgod@users.noreply.github.com>
2021-04-08 22:31:47 +0200

    Create image_processing.py
    load_itk, save_itk

---
+++ image_processing.py
@@ -0,0 +1,33 @@
+
+def load_itk(filename, require_sp_po=False):
+    # Reads the image using SimpleITK
+    if os.path.isfile(filename):
+        itkimage = sitk.ReadImage(filename)
+    else:
+        print('nonfound:', filename)
+        return [], [], []
+
+    # Convert the image to a  numpy array first ands then shuffle the dimensions to get axis in the order z,y,x
+    ct_scan = sitk.GetArrayFromImage(itkimage)
+
+    # ct_scan[ct_scan>4] = 0 #filter trachea (label 5)
+    # Read the origin of the ct_scan, will be used to convert the coordinates from world to voxel and vice versa.
+    origin = np.array(list(reversed(itkimage.GetOrigin())))
+
+    # Read the spacing along each dimension
+    spacing = np.array(list(reversed(itkimage.GetSpacing())))
+    #     print('get_orientation', get_orientation)
+    if require_sp_po:
+        return ct_scan, origin, spacing
+    else:
+        return ct_scan
+
+
+def save_itk(filename, scan, origin, spacing, dtype='int16'):
+    stk = sitk.GetImageFromArray(scan.astype(dtype))
+    stk.SetOrigin(origin[::-1])
+    stk.SetSpacing(spacing[::-1])
+
+    writer = sitk.ImageFileWriter()
+    writer.Execute(stk, filename, True)
+
